ce7b0e9fdd43b064ca1e082d9e91805311b66b91
jimakoji <jima0537@gmail.com>
2013-12-06 23:33:42 +0900

    Add user following

--- app/assets/javascripts/js_for_micropost.js
+++ app/assets/javascripts/js_for_micropost.js
@@ -5,7 +5,7 @@
 */
 
 $(function(){
-if ($('#post_field').val().length == 0) {
+if ($('#micropost_content').val().length == 0) {
       $('#count').html("140文字入力できます。" );
 }else{
       var n = 140 - $('#post_field').val().length;
@@ -14,7 +14,7 @@ if ($('#post_field').val().length == 0) {
 });
 
 $(function(){
-    $('#post_field').bind('keydown keyup keypress change',function(){
+    $('#micropost_content').bind('keydown keyup keypress change',function(){
         var thisValueLength = 140 - $(this).val().length;
         thisValueLength == 140? str = "" :str = "あと"
         $('#count').html( str + thisValueLength + "文字入力できます。" );
